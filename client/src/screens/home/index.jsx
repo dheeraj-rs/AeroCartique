1750df8f9aaa723c3638f4b5a0e96c7e6443b4d0
dheeraj <drjsde@gmail.com>
2023-11-04 14:44:07 +0530

    Add Home screen image and title

--- client/src/screens/home/index.jsx
+++ client/src/screens/home/index.jsx
@@ -1,10 +1,17 @@
-import "./styles/Home.css"
+import './styles/Home.css';
+
 function Home() {
   return (
-    <section className="Home-container">
-      <h1>Home</h1>
+    <section className="home-container">
+      <div className="background-image">
+        <div className="home-text">
+          <h1>Elevate your Space with Royal Furniture</h1>
+          <p>Transform your home into a stylish and comfortable living space with Lanima Furniture.</p>
+          <button className="btn">View Collection</button>
+        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Home
+export default Home;
